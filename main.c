8c15e66f97b41a94839b7e56195f5f78dddec4f2
sujana <sujana.gutta@eagles.oc.edu>
2017-12-10 11:56:29 -0600

    Final commit

--- main.c
+++ main.c
@@ -1,58 +1,50 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <omp.h>
-
-# define NPOINTS 1000
-# define MAXITER 1000
-
-void testpoint(void);
-
-struct d_complex{
-    double r;
-    double i;
-};
-
-struct d_complex c;
-int numoutside = 0;
-
-int main(){
-    int i, j;
-    double area, error, eps  = 1.0e-5;
-
-#pragma omp parallel for default(shared) private(c,eps)
-    for (i=0; i<NPOINTS; i++) {
-        for (j=0; j<NPOINTS; j++) {
-            c.r = -2.0+2.5*(double)(i)/(double)(NPOINTS)+eps;
-            c.i = 1.125*(double)(j)/(double)(NPOINTS)+eps;
-            testpoint();
-        }
-    }
-
-
-    area=2.0*2.5*1.125*(double)(NPOINTS*NPOINTS-numoutside)/(double)(NPOINTS*NPOINTS);
-    error=area/(double)NPOINTS;
-
-    printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
-
+#include<stdio.h>
+#include<conio.h>
+#include<openacc.h>
+#include<openmp.h>
+float func(float x,float y);
+void main()
+{
+ float sx,ex,yp,h,i,yp1,s,s1,s2,s3,s4;
+ clrscr();
+ printf("Enter the value of Y(1) ::");
+ scanf("%f",&yp);
+ printf("Enter the value of Start x ::");
+ scanf("%f",&sx);
+ printf("Enter the value of End x ::");
+ scanf("%f",&ex);
+ printf("Enter the value of h ::");
+ scanf("%f",&h);
+ 
+#ifdef openacc
+#pragma openacc loop kernels reduce (:*)
+#pragma openacc independent
+#elif omp
+#pragma omp for parallel reduce (:*)
+#pragma omp single
+#endif
+for(i=sx;i<=ex+h;i=i+h)
+ {
+  s1 = i*yp;
+  s2 = (i+(h/2))*(yp+((h/2)*s1));
+  s3 = (i+(h/2))*(yp+((h/2)*s2));
+  s4 = (i+h)*(yp+(h*s3));
+  s = (s1+(2*s2)+(2*s3)+s4)/6;
+  yp1 = yp + (h*s);
+   if((int)(i*10)==(int)(ex*10))
+ 
+#ifdef openacc
+#pragma openacc wait
+#elif omp
+#pragma omp barrier
+#pragma omp master
+#endif
+{
+    printf("\nx=%.4f\ty=%.4f",i,yp);
+   else
+    printf("\nx=%.4f\ty=%.4f\ty1=%.4f\th=%.4f\ts=%.4f",i,yp,yp1,h,s);
 }
-
-void testpoint(void){
-
-    struct d_complex z;
-    int iter;
-    double temp;
-
-    z=c;
-    for (iter=0; iter<MAXITER; iter++){
-        temp = (z.r*z.r)-(z.i*z.i)+c.r;
-        z.i = z.r*z.i*2+c.i;
-        z.r = temp;
-        if ((z.r*z.r+z.i*z.i)>4.0) {
-            numoutside++;
-            break;
-        }
-    }
-
+  yp = yp1;
+ }
+  return 0;
 }
